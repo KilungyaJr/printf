2053079c11f1ef0d3fa2bc15505be27efb342198
vashty <vashtylavilavi50@gmail.com>
2022-08-09 23:49:49 +0300

    updated print_int.c file

--- print_int.c
+++ print_int.c
@@ -66,3 +66,21 @@ int count_digit(int i)
 	}
 	return (d);
 }
+
+/**
+ * print_binary - prints a number in base 2
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct that determines
+ * if a flag is passed to _printf
+ * Description: the function calls convert() which in turns converts the input
+ * number into the correct base and returns it as a string
+ * Return: the number of char printed
+ */
+int print_binary(va_list l, flags_t *f)
+{
+	unsigned int num = va_arg(l, unsigned int);
+	char *str = convert(num, 2, 0);
+
+	(void)f;
+	return (_puts(str));
+}
