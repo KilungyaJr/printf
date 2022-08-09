9467e07030f7b5799a67880f43a1dc22b72090cc
vashty <vashtylavilavi50@gmail.com>
2022-08-09 21:22:56 +0300

    updated main.h

--- main.h
+++ main.h
@@ -9,4 +9,19 @@ int _printf(const char *format, ...);
 int print_char(va_list l, flags_t *f);
 int print_string(va_list l, flags_t *f);
 
+/*print_int function*/
+int print_int(va_list l, flags_t *f);
+
+/*print_perce function*/
+int print_perce(va_list l, flags_t *f);
+
+/*get_print function*/
+int (*get_print(char s))(va_list, flags_t *);
+
+/*get_flag.c function*/
+int get_flag(char s, flags_t *f);
+
+
+
+
 #endif /* ifndef MAIN_H */
