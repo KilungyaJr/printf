5cf6e9814acba7348a5dff87cbdf6f79131c84ed
vashty <vashtylavilavi50@gmail.com>
2022-08-09 21:23:21 +0300

    created get_print.c file

---
+++ get_print.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * get_print - selects the right printing function
+ * depending on the conversion specifier passed to _printf
+ * @s: character that holds the conversion specifier
+ * Description: the function loops through the structs array
+ * func_arr[] to find a match between the specifier passed to _printf
+ * and the first element of the struct, and then the approriate
+ * printing function
+ * Return: a pointer to the matching printing function
+ */
+int (*get_print(char s))(va_list, flags_t *)
+{
+	ph func_arr[] = {
+		{'i', print_int},
+		{'s', print_string},
+		{'c', print_char},
+		{'d', print_int},
+		{'%', print_percent}
+		};
+	int flags = 5;
+
+	register int i;
+
+	for (i = 0; i < flags; i++)
+		if (func_arr[i].c == s)
+			return (func_arr[i].f);
+	return (NULL);
+}
