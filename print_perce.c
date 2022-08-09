6082b1f6d77176840ea93cf622bacd6f7518cdd2
Silvester Kilungya <silvesterkilungya@gmail.com>
2022-08-09 22:33:42 +0300

    updated prototype name to match other files

--- print_perce.c
+++ print_perce.c
@@ -2,12 +2,12 @@
 
 
 /**
- * print_perce - prints a percent
+ * print_percent - prints a percent
  * @l: va_list arguments from _printf
  * @f: pointer to the struct flags in which we turn the flags on
  * Return: number of char printed
  */
-int print_perce(va_list l, flags_t *f)
+int print_percent(va_list l, flags_t *f)
 {
 	(void)l;
 	(void)f;
