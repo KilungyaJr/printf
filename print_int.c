b5f7fb6ae45aefdf8f1fa78b8a834554a24506d0
Silvester Kilungya <silvesterkilungya@gmail.com>
2022-08-09 22:34:55 +0300

    created two functions print number and count digit

--- print_int.c
+++ print_int.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
  * print_int - prints an integer
  * @l: va_list of arguments from _printf
@@ -21,4 +20,49 @@ int print_int(va_list l, flags_t *f)
 		res++;
 	print_number(n);
 	return (res);
+}
+
+/**
+ * print_number - helper function that loops through
+ * an integer and prints all its digits
+ * @n: integer to be printed
+ */
+void print_number(int n)
+{
+	unsigned int n1;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n1 = -n;
+	}
+	else
+		n1 = n;
+
+	if (n1 / 10)
+		print_number(n1 / 10);
+	_putchar((n1 % 10) + '0');
+}
+
+/**
+ * count_digit - returns the number of digits in an integer
+ * for _printf
+ * @i: integer to evaluate
+ * Return: number of digits
+ */
+int count_digit(int i)
+{
+	unsigned int d = 0;
+	unsigned int u;
+
+	if (i < 0)
+		u = i * -1;
+	else
+		u = i;
+	while (u != 0)
+	{
+		u /= 10;
+		d++;
+	}
+	return (d);
 }
